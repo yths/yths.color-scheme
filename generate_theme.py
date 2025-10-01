d807cb7d1b7e83b2f0086469dde59acd03c3c763
Yannik Schelske <home@yths.de>
2025-10-01 06:45:51 +0200

    feat: add color scheme to theme mapping

--- generate_theme.py
+++ generate_theme.py
@@ -19,6 +19,30 @@ if __name__ == "__main__":
     theme["light"]["grey"] = color_dict["grey"][0]
     theme["light"]["cursor"] = color_dict["black"][0]
 
+    theme["light"]["red"] = color_dict["secondary_red"][0]
+    theme["light"]["green"] = color_dict["secondary_green"][0]
+    theme["light"]["blue"] = color_dict["secondary_blue"][0]
+    theme["light"]["yellow"] = color_dict["secondary_yellow"][0]
+    theme["light"]["magenta"] = color_dict["secondary_magenta"][0]
+    theme["light"]["cyan"] = color_dict["secondary_cyan"][0]
+
+    theme["light"]["pastel_red"] = color_dict["secondary_red_muted"][0]
+    theme["light"]["pastel_green"] = color_dict["secondary_green_muted"][0]
+    theme["light"]["pastel_blue"] = color_dict["secondary_blue_muted"][0]
+    theme["light"]["pastel_yellow"] = color_dict["secondary_yellow_muted"][0]
+    theme["light"]["pastel_magenta"] = color_dict["secondary_magenta_muted"][0]
+    theme["light"]["pastel_cyan"] = color_dict["secondary_cyan_muted"][0]
+
+    theme["light"]["light_muted"] = color_dict["white_dark"][0]
+    theme["light"]["dark_muted"] = color_dict["black_light"][0]
+    theme["light"]["light_grey"] = color_dict["grey_light"][0]
+    theme["light"]["dark_grey"] = color_dict["grey_dark"][0]
+
+    theme["light"]["effect_bright"] = color_dict["effect_bright"][0]
+    theme["light"]["effect_dark"] = color_dict["effect_dark"][0]
+    theme["light"]["effect_muted"] = color_dict["secondary_effect"][0]
+    theme["light"]["effect_pastel"] = color_dict["secondary_effect_muted"][0]
+
     theme["dark"]["positive"] = color_dict["primary_green"][0]
     theme["dark"]["negative"] = color_dict["primary_red"][0]
     theme["dark"]["neutral"] = color_dict["effect"][0]
@@ -26,6 +50,30 @@ if __name__ == "__main__":
     theme["dark"]["background"] = color_dict["black"][0]
     theme["dark"]["grey"] = color_dict["grey"][0]
     theme["dark"]["cursor"] = color_dict["white"][0]
+
+    theme["dark"]["red"] = color_dict["secondary_red_muted"][0]
+    theme["dark"]["green"] = color_dict["secondary_green_muted"][0]
+    theme["dark"]["blue"] = color_dict["secondary_blue_muted"][0]
+    theme["dark"]["yellow"] = color_dict["secondary_yellow_muted"][0]
+    theme["dark"]["magenta"] = color_dict["secondary_magenta_muted"][0]
+    theme["dark"]["cyan"] = color_dict["secondary_cyan_muted"][0]
+
+    theme["dark"]["pastel_red"] = color_dict["secondary_red"][0]
+    theme["dark"]["pastel_green"] = color_dict["secondary_green"][0]
+    theme["dark"]["pastel_blue"] = color_dict["secondary_blue"][0]
+    theme["dark"]["pastel_yellow"] = color_dict["secondary_yellow"][0]
+    theme["dark"]["pastel_magenta"] = color_dict["secondary_magenta"][0]
+    theme["dark"]["pastel_cyan"] = color_dict["secondary_cyan"][0]
+
+    theme["dark"]["light_muted"] = color_dict["black_light"][0]
+    theme["dark"]["dark_muted"] = color_dict["white_dark"][0]
+    theme["dark"]["light_grey"] = color_dict["grey_dark"][0]
+    theme["dark"]["dark_grey"] = color_dict["grey_light"][0]
+
+    theme["dark"]["effect_bright"] = color_dict["effect_dark"][0]
+    theme["dark"]["effect_dark"] = color_dict["effect_bright"][0]
+    theme["dark"]["effect_muted"] = color_dict["secondary_effect_muted"][0]
+    theme["dark"]["effect_pastel"] = color_dict["secondary_effect"][0]
 
     with open(os.path.join("data", "output", "theme.pkl"), "wb") as output_handle:
         pickle.dump(theme, output_handle)
